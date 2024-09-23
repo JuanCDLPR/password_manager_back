612326e4b6b69b02fe4fa6c13b48f000d084dabd
Juan De La Paz <JuanCDLPR@Outlook.com>
2024-09-22 18:49:45 -0600

    actualizacion y eliminacion de grupo

--- controllers/grupos.controller.js
+++ controllers/grupos.controller.js
@@ -1,14 +1,14 @@
 const { response, request } = require("express");
 const mongoose = require("mongoose");
 const { Respuesta } = require("../models/repuesta");
-const GruposSchema = require("../models/grupos.model");
+const GruposModel = require("../models/grupos.model");
 
 const insertar = async (req = request, res = response) => {
   try {
     const { uid } = req;
     const { nombre, acron } = req.body;
 
-    const NewGrupo = new GruposSchema({
+    const NewGrupo = new GruposModel({
       id_usuario: uid,
       name: nombre,
       acronimo: acron,
@@ -57,7 +57,7 @@ const listar = async (req = request, res = response) => {
         ? { name: { $regex: query, $options: "i" }, id_usuario: uid }
         : { id_usuario: uid };
 
-    const DataGrupo = await GruposSchema.find(query_find).sort(ordenamiento);
+    const DataGrupo = await GruposModel.find(query_find).sort(ordenamiento);
 
     return res
       .status(200)
@@ -70,7 +70,96 @@ const listar = async (req = request, res = response) => {
   }
 };
 
+const eliminar = async (req = request, res = response) => {
+  try {
+    const { ID } = req.query;
+
+    const RegistroAEliminar = await GruposModel.findById(ID);
+
+    if (!RegistroAEliminar) {
+      return res
+        .status(200)
+        .json(
+          Respuesta(900, "OK", "No se encontro la plataforma a eliminar", [])
+        );
+    }
+
+    await GruposModel.findByIdAndDelete(RegistroAEliminar._id);
+
+    return res
+      .status(200)
+      .json(Respuesta(200, "OK", "Eliminado correctamente", []));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json(Respuesta(500, "ERROR", "Error al eliminar", []));
+  }
+};
+
+const consultar = async (req = request, res = response) => {
+  try {
+    const { ID } = req.query;
+
+    const DatosGrupo = await GruposModel.findById(ID);
+
+    if (!DatosGrupo) {
+      return res
+        .status(200)
+        .json(
+          Respuesta(900, "OK", "No se encontro informacio de la plataforma", [])
+        );
+    }
+
+    return res
+      .status(200)
+      .json(Respuesta(200, "OK", "Eliminado correctamente", [DatosGrupo]));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json(Respuesta(500, "ERROR", "Error al eliminar", []));
+  }
+};
+
+const actualizar = async (req = request, res = response) => {
+  try {
+    const { id, nombre, acron } = req.body;
+
+    const GrupoActualizado = await GruposModel.findByIdAndUpdate(
+      id,
+      { name: nombre, acronimo: acron, actualizado: Date.now() },
+      { new: true }
+    );
+
+    if (!GrupoActualizado) {
+      return res
+        .status(200)
+        .json(
+          Respuesta(
+            900,
+            "OK",
+            "No se encontro informacion de la plataforma",
+            []
+          )
+        );
+    }
+
+    return res
+      .status(200)
+      .json(Respuesta(200, "OK", "Actualizado correctamente", []));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json(Respuesta(500, "ERROR", "Error al eliminar", []));
+  }
+};
+
 module.exports = {
   insertar,
   listar,
+  eliminar,
+  consultar,
+  actualizar,
 };
--- routes/grupos.routes.js
+++ routes/grupos.routes.js
@@ -1,11 +1,20 @@
 const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jws");
 
-const { insertar, listar } = require("../controllers/grupos.controller");
+const {
+  insertar,
+  listar,
+  eliminar,
+  consultar,
+  actualizar,
+} = require("../controllers/grupos.controller");
 
 const router = Router();
 
 router.post("/insertar", [validarJWT], insertar);
 router.get("/listar", [validarJWT], listar);
+router.post("/eliminar", [validarJWT], eliminar);
+router.get("/consultar", [validarJWT], consultar);
+router.post("/actualizar", [validarJWT], actualizar);
 
 module.exports = { grupos: router };
